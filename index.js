3d348fdce63385a7c517731c7e90beb2406266fc
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2023-03-27 21:10:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // Using mongodb atlas to connect to  the db
-mongoose.connect(`mongodb+srv://sachinyadav1469:Sachin%40123@cluster0.my3twen.mongodb.net/buyume?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://sachinyadav1469@cluster0.my3twen.mongodb.net/buyume?retryWrites=true&w=majority`)
         .then(result=>{
             console.log("Connected to db")
             app.listen(8000);
